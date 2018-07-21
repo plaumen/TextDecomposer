688f6d635d3cc5faad57af50bf6592e2a2284eee
plaumen <przemek.ciszak@gmail.com>
2018-07-21 12:29:19 +0200

    Add model classes: sentence and text

---
+++ TextDecomposer/Models/Sentence.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace TextDecomposer.Models
+{
+    public class Sentence
+    {
+        [XmlElement("word")]
+        public List<string> Words { get; set; }
+    }
+}
---
+++ TextDecomposer/Models/Text.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace TextDecomposer.Models
+{
+    public class Text
+    {
+        [XmlElement("sentence")]
+        public List<Sentence> Sentences { get; set; }
+    }
+}
